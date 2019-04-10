6674d421983e4154eeb5ed073cf51e3423b082a5
Nils Dornbusch <Nils.Dornbusch@DLR.de>
2019-04-10 09:50:09 +0200

    new plot

--- FEniCSSimulation.py
+++ FEniCSSimulation.py
@@ -57,7 +57,7 @@ class FEniCSSimulation:
         """define the variational problem"""
         dt = 0.1
         sigma_int = interpolate(self.sigma, self.V[1])
-        F = self.u[0]*self.v[0]*dx-self.mu*dt*dot(grad(self.u[0]),grad(self.v[0]))*dx-(self.u_n*self.v[0])*dx\
+        F = self.u[0]*self.v[0]*dx+self.mu*dt*dot(grad(self.u[0]),grad(self.v[0]))*dx-(self.u_n*self.v[0])*dx\
             -dt*(self.gradP*self.v[0])*dx+ dt*dot(sigma_int,grad(self.v[0]))*dx
             #
         self.a = lhs(F)
